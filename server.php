9947cee560b2ba7b81ddc29523b4aa3826e3edf7
Francesca Barletta <wallebar@gmail.com>
2024-05-03 14:38:10 +0200

    add screen print in server.php

--- server.php
+++ server.php
@@ -1,3 +1,11 @@
+<?php
+var_dump($_GET);
+$message = $_GET['message'];
+$message_length = strlen($message);
+$new_message = str_replace('Lorem','***',$message);
+$new_message_length = strlen($new_message);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,5 +15,13 @@
 </head>
 <body>
    <h1>ciao</h1>
+   <h2>Il tuo messaggio è:</h2>
+   <p><?php echo $message; ?></p>
+   <h3>La lunghezza del messaggio è:</h3>
+   <p><?php echo $message_length ?></p>
+   <h3>Il messaggio con la parola nascosta è:</h3>
+   <p><?php echo $new_message ?></p>
+   <h3>La lunghezza del messaggio con la parola nascosta è:</h3>
+   <p><?php echo $new_message_length ?></p>
 </body>
 </html>
